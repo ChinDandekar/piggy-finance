aebc123605697966cf4970be3ac89e968d13f17b
ChinDandekar <102712851+ChinDandekar@users.noreply.github.com>
2024-01-10 22:13:06 -0800

    Added custom adapter

---
+++ backend/api/adapter.py
@@ -0,0 +1,11 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+import logging
+
+logger = logging.getLogger(__name__)
+
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
+    def authentication_error(self, request, provider_id, error, exception, extra_context):
+        logger.error(
+            f'SocialAccount authentication error:\n request: {request},\n provider: {provider_id},\n error: {error},\n exception: {exception},\n extra_context: {extra_context}'
+        )
+        return super().authentication_error(request, provider_id, error, exception, extra_context)
