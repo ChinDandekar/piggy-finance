cd43eb25e93e33472cf414ce0f63a79eaa5ded7f
ChinDandekar <102712851+ChinDandekar@users.noreply.github.com>
2024-02-05 00:10:36 -0800

    removing pdb from adapter.py

--- backend/api/adapter.py
+++ backend/api/adapter.py
@@ -6,7 +6,6 @@ logger = logging.getLogger(__name__)
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def authentication_error(self, request, provider_id, error, exception, extra_context):
-        pdb.set_trace()
         logger.error(
             f'SocialAccount authentication error:\n request: {request},\n provider: {provider_id},\n error: {error},\n exception: {exception},\n extra_context: {extra_context}'
         )
